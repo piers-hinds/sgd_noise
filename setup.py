5fc245858513a3906f402b7069ecfbf707d9dda7
Piers <61315972+piers-hinds@users.noreply.github.com>
2022-10-06 12:10:23 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sgd_noise',
+    url='https://github.com/piers-hinds/sgd_noise',
+    author='Piers Hinds',
+    author_email='pmxph7@nottingham.ac.uk',
+    packages=['sgd_noise'],
+    install_requires=['numpy', 'torch', 'pandas'],
+    tests_require=['pytest'],
+    version='0',
+    description='NA'
+)
