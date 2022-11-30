9dc337eff7b7efcc283b445861b89c841355ec3e
Piers <61315972+piers-hinds@users.noreply.github.com>
2022-11-30 12:06:38 +0000

    weighted gradients

--- sgd_noise/train.py
+++ sgd_noise/train.py
@@ -3,41 +3,33 @@ import pandas as pd
 import numpy as np
 
 
-def train(model, opt, criterion, epochs, dl, vdl=None, metrics=[], eval_train_loss=True, print_losses=True):
+def weighted_backward(model, losses):
+    for loss in losses:
+        model.alpha = loss / (losses.norm() * np.sqrt(len(losses)))
+        loss.backward(retain_graph=True)
+
+
+def train_one_epoch(model, opt, criterion, dl, wgrad=False):
+    model_device = next(model.parameters()).device
+    runnning_loss = 0
     model.train()
-    losses = []
-    val_losses = []
-    for epoch in range(epochs):
-        running_loss = 0
-        for x, y in dl:
-            opt.zero_grad()
-            preds = model(x.to(next(model.parameters()).device))
-            loss = criterion(preds, y.to(next(model.parameters()).device))
+    for x, y in dl:
+        opt.zero_grad()
+        output = model(x.to(model_device))
+        losses = criterion(output, y.to(model_device))
+        if wgrad:
+            weighted_backward(model, losses)
+            runnning_loss += losses.mean().item()
+        else:
+            loss = losses.mean()
             loss.backward()
-            opt.step()
-            running_loss += loss.item()
-
-        if eval_train_loss:
-            train_loss = validate_model(model, dl, metrics)[0][0]
-        else:
-            train_loss = running_loss / len(dl)
-        losses.append(train_loss)
-
-        if vdl is not None:
-            val_loss, _ = validate_model(model, vdl, metrics)
-            model.train()
-            if print_losses:
-                print('Epoch: ', epoch, '    Train loss: ', round(train_loss, 6),
-                      '    Val loss: ', *[round(v, 6) for v in val_loss])
-
-        else:
-            if print_losses:
-                print('Epoch: ', epoch, '    Train loss: ', round(train_loss, 6))
-        val_losses.append(val_loss[0])
-    return torch.tensor(losses), torch.tensor(val_losses)
+            runnning_loss += loss.item()
+        opt.step()
+    return runnning_loss / len(dl)
 
 
 def validate_model(model, dl, metrics, save_preds=False):
+    model_device = next(model.parameters()).device
     if not isinstance(metrics, list):
         metrics = [metrics]
     dfs = []
@@ -45,9 +37,9 @@ def validate_model(model, dl, metrics, save_preds=False):
     with torch.inference_mode():
         running_loss = np.zeros(shape=len(metrics))
         for x, y in dl:
-            preds = model(x.to(next(model.parameters()).device))
+            preds = model(x.to(model_device))
             for i, metric in enumerate(metrics):
-                loss = metric(preds, y.to(next(model.parameters()).device))
+                loss = metric(preds, y.to(model_device))
                 running_loss[i] += loss.item()
             if save_preds:
                 dfs.append(pd.DataFrame({'pred': preds.cpu().numpy()}))
@@ -56,3 +48,33 @@ def validate_model(model, dl, metrics, save_preds=False):
         return running_loss / len(dl), all_preds
     else:
         return running_loss / len(dl), None
+
+
+def train(model, opt, criterion, epochs, dl, vdl=None, metrics=[], wgrad=False, eval_train_loss=True,
+          print_losses=True):
+    if wgrad:
+        assert hasattr(model, 'alpha'), 'If wgrad=True, model must be WeightedGradient'
+
+    train_losses, val_losses = [], []
+
+    for epoch in range(epochs):
+        epoch_loss = train_one_epoch(model, opt, criterion, dl, wgrad)
+        if eval_train_loss:
+            train_loss = validate_model(model, dl, metrics)[0][0]
+        else:
+            train_loss = epoch_loss
+        train_losses.append(train_loss)
+
+        if vdl is not None:
+            val_loss, _ = validate_model(model, vdl, metrics)
+            val_losses.append(val_loss[0])
+            model.train()
+            if print_losses:
+                print('Epoch: ', epoch, '    Train loss: ', round(train_loss, 6),
+                      '    Val loss: ', *[round(v, 6) for v in val_loss])
+
+        else:
+            if print_losses:
+                print('Epoch: ', epoch, '    Train loss: ', round(train_loss, 6))
+
+    return torch.tensor(train_losses), torch.tensor(val_losses)
